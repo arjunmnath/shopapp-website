48e321e8e6c77d71007aaebb675f3ad6c50c6371
arjunmnath <arjunmanjunath80@gmail.com>
2023-07-29 22:46:01 +0530

    fix

--- app/console/products/table.tsx
+++ app/console/products/table.tsx
@@ -178,83 +178,84 @@ const columns: ColumnDef<Product>[] = [
     }, {
         id: "actions",
         enableHiding: false,
-        cell: ({ row }) => {
-            const [open, setOpen] = useState(false)
-            const dropdownRef = useRef(null)
-            const [alertOpen, setAlertOpen] = useState(false)
-            const stateContext = useContext(DataContext)
-            const { toast } = useToast()
-            return (<AlertDialog open={alertOpen} onOpenChange={(open) => setAlertOpen(open)}>
-                <DropdownMenu key={row.getValue('productId')}
-                    open={open} onOpenChange={(open) => {
-                        setOpen(open)
-                    }}>
-                    <DropdownMenuTrigger ref={dropdownRef} asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <DotsHorizontalIcon className="h-4 w-4" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent key={row.getValue('productId')} align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(`Name: ${row.getValue('name')}\n Price: ${row.getValue('price')}`)}
-                        >
-                            Copy Product Details
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Show Stock History</DropdownMenuItem>
-                        <DropdownMenuItem>Edit Price</DropdownMenuItem>
-                        <DropdownMenuItem>Add New Supply Batch</DropdownMenuItem>
-                        <DropdownMenuItem onClick={e => setAlertOpen(true)}>Delete</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                <AlertDialogContent onCloseAutoFocus={(event) => {
-                    // dropdownRef.current?.focus();
-                    event.preventDefault();
-                }}>
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            This action cannot be undone. This will permanently delete your
-                            account and remove your data from our servers.
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                        <AlertDialogCancel onClick={e => setAlertOpen(false)}>Cancel</AlertDialogCancel>
-                        <AlertDialogAction onClick={async () => {
-                            const productId: string = row.getValue('productId')
-                            stateContext.setProductData(stateContext.productData.filter((ele) => ele.productId != productId))
-                        toast({
-                            title: "Deleting..."
-                            })
-
-                        const success = await deleteProductAction({clientToken: localStorage.getItem('clitkn'), productId: productId, setdata: stateContext.fetchProductData })
-                        console.log(success)
-                        if (success) {
-                            toast({
-                                title: "Good Job!!",
-                                description: "Product deleted successfully"
-                            })
-                        } else {
-                            toast({
-                                title: "Oops!!",
-                                description: "Something Went Wrong",
-                                variant: 'destructive'
-                            })
-                        }
-
-                        }}>Continue</AlertDialogAction>
-                </AlertDialogFooter>
-            </AlertDialogContent>
-            </AlertDialog >
-            )
-        },
+        cell: ({ row }) => (<TableActions row={row}/>)
     },
 
 
 
 ]
+
+const TableActions = (props:{row:any}) => {
+    const [open, setOpen] = useState(false)
+    const dropdownRef = useRef(null)
+    const [alertOpen, setAlertOpen] = useState(false)
+    const stateContext = useContext(DataContext)
+    const { toast } = useToast()
+    return (<AlertDialog open={alertOpen} onOpenChange={(open) => setAlertOpen(open)}>
+        <DropdownMenu key={props.row.getValue('productId')}
+            open={open} onOpenChange={(open) => {
+                setOpen(open)
+            }}>
+            <DropdownMenuTrigger ref={dropdownRef} asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <DotsHorizontalIcon className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent key={props.row.getValue('productId')} align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                    onClick={() => navigator.clipboard.writeText(`Name: ${row.getValue('name')}\n Price: ${row.getValue('price')}`)}
+                >
+                    Copy Product Details
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Show Stock History</DropdownMenuItem>
+                <DropdownMenuItem>Edit Price</DropdownMenuItem>
+                <DropdownMenuItem>Add New Supply Batch</DropdownMenuItem>
+                <DropdownMenuItem onClick={e => setAlertOpen(true)}>Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+        <AlertDialogContent onCloseAutoFocus={(event) => {
+            // dropdownRef.current?.focus();
+            event.preventDefault();
+        }}>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete your
+                    account and remove your data from our servers.
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel onClick={e => setAlertOpen(false)}>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={async () => {
+                    const productId: string = props.row.getValue('productId')
+                    stateContext.setProductData(stateContext.productData.filter((ele) => ele.productId != productId))
+                toast({
+                    title: "Deleting..."
+                    })
+
+                const success = await deleteProductAction({clientToken: localStorage.getItem('clitkn'), productId: productId, setdata: stateContext.fetchProductData })
+                console.log(success)
+                if (success) {
+                    toast({
+                        title: "Good Job!!",
+                        description: "Product deleted successfully"
+                    })
+                } else {
+                    toast({
+                        title: "Oops!!",
+                        description: "Something Went Wrong",
+                        variant: 'destructive'
+                    })
+                }
+
+                }}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+    </AlertDialogContent>
+    </AlertDialog >)
+}
 
 
 export {
